b0dbb47234e84df72a81158d738114379a6dee4d
akshay <akshayjamwal2016@gmail.com>
2021-08-21 22:31:02 +0530

    Add project files.

---
+++ Middlewares/MyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger logger;
+
+        public MyMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            logger = loggerFactory.CreateLogger("MyLogger");
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            logger.LogInformation("Test log");
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddleware>();
+        }
+    }
+}
